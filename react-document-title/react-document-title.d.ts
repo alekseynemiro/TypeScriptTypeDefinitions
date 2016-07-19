5f21fcad50046592191631a6ea3fd83d4c12c962
Aleksey Nemiro <aleksey@kbyte.ru>
2016-07-19 17:39:13 +0300

    import * as React from 'react'

--- react-document-title/react-document-title.d.ts
+++ react-document-title/react-document-title.d.ts
@@ -2,6 +2,8 @@
 
 declare module 'react-document-title' {
 
+  import * as React from 'react';
+
   export default class DocumentTitle extends React.Component<any, any> {
 
     public title: string;
