b587eb29f2b552cbb289b06f667d40cdf0b9b887
Aleksey Nemiro <aleksey@kbyte.ru>
2016-07-19 17:35:53 +0300

    Reference to react.

--- react-document-title/react-document-title.d.ts
+++ react-document-title/react-document-title.d.ts
@@ -1,4 +1,6 @@
-﻿declare module 'react-document-title' {
+﻿/// <reference path="../react/react.d.ts" />
+
+declare module 'react-document-title' {
 
   export default class DocumentTitle extends React.Component<any, any> {
 
