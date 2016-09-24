a2ef1df3e7a053fed9b525e2a5da140f6376dad2
Aleksey Nemiro <aleksey@kbyte.ru>
2016-09-24 17:54:58 +0300

    react-bootstrap-typeahead

---
+++ react-bootstrap-typeahead/react-bootstrap-typeahead.d.ts
@@ -0,0 +1,37 @@
+﻿// Type definitions for react-bootstrap-typeahead v0.9.2
+// Project: https://github.com/ericgio/react-bootstrap-typeahead
+// Definitions by: Aleksey Nemiro <https://github.com/alekseynemiro>
+
+/// <reference path="../react/react.d.ts" />
+
+/**
+ * https://github.com/ericgio/react-bootstrap-typeahead
+ */
+declare module 'react-bootstrap-typeahead' {
+
+  import React = __React;
+
+  namespace Typeahead {
+
+    export interface Props {
+
+      onChange: () => void;
+
+      /** Array list or objects. */
+      options: Array<any>;
+
+      multiple?: boolean;
+
+      filterBy?: () => void;
+
+      renderMenuItemChildren?: (props, option, idx) => void;
+
+      renderToken?: (option, onRemove) => void;
+
+    }
+
+  }
+
+  export default class Typeahead extends React.Component<Typeahead.Props, any> { }
+
+}
