9655caf6d89d4ca171cccf31c54533bba9fedc6f
Aleksey Nemiro <aleksey@kbyte.ru>
2016-12-04 14:55:16 +0300

    jsx-to-html

---
+++ jsx-to-html/jsx-to-html.d.ts
@@ -0,0 +1,13 @@
+﻿// Type definitions for jsx-to-html v1.1.0
+// Project: https://github.com/OpenJSX/jsx-to-html
+// Definitions by: Aleksey Nemiro <https://github.com/alekseynemiro>
+
+declare namespace JsxToHtml {
+
+  export function render(value: JSX.Element): string;
+
+}
+
+declare module 'jsx-to-html' {
+  export = JsxToHtml;
+}
