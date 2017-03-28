ee1bbcf6f2cec1d506f9ef7f34d1bf8532879f7b
Aleksey Nemiro <aleksey@kbyte.ru>
2017-03-28 10:53:58 +0300

    yandex-metrika-settings

---
+++ yandex-metrika-settings/index.d.ts
@@ -0,0 +1,27 @@
+﻿export interface YaMetrikaSettings {
+
+  /** Идентификатор счетчика. */
+  id: number;
+
+  /** Карта кликов. */
+  clickmap?: boolean;
+
+  /** Пути ссылок. */
+  trackLinks?: boolean;
+
+  /***/
+  accurateTrackBounce?: boolean;
+
+  /** Вебвизор, карта скроллинга, аналитика форм. */
+  webvisor: boolean;
+
+  /** Отслеживание хеша в адресной строке браузера. */
+  trackHash: boolean;
+
+  /** Параметры отправки на индексацию страниц сайта. */
+  ut: { 'noindex' };
+
+  /** Отправка в Метрику данных электронной коммерции. */
+  ecommerce: string;
+
+}
